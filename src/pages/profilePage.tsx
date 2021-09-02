9eb7002ca86af53560e8463e5f26ec6b871a320a
David Jensen <tsourdox@icloud.com>
2021-09-02 14:42:47 +0200

    create profile page

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,7 +1,33 @@
+import { CSSProperties, useContext } from 'react';
+import ImageCard from '../components/ImageCard';
+import { ImageContext } from '../contexts/imageContext';
+import { ImageData, images } from '../data';
 
 const ProfilePage = () => {
+    const { likedImages } = useContext(ImageContext);
+    
+    // Link-ish way of doing an intersection
+    const likedImageList = images.filter(image =>
+        likedImages.includes(image.id));
 
-    return <p>Profile Page</p>;
+    // Classic way of doing an intersection
+    // const newList: ImageData[] = [];
+    // for (const image of images) {
+    //     if (likedImages.includes(image.id)) {
+    //         newList.push(image);
+    //     }
+    // }
+
+    return (
+        <div style={rootStyle}>
+            {likedImageList.map(image => <ImageCard key={image.id} image={image} />)}
+        </div>
+    );
 }
+
+const rootStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column'
+};
 
 export default ProfilePage;
